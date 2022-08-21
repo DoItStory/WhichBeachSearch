b3b2538d06aa882111a3c4ef4ffc045c4c90503c
winovation <gwon.innovation@gmail.com>
2022-08-21 22:06:35 +0900

    fix: 예외처리 추가

--- js/signUp.js
+++ js/signUp.js
@@ -4,17 +4,7 @@ import {
   createUserWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
-export function signUpWithEmailPassword(email, password) {
+export async function signUpWithEmailPassword(email, password) {
   const auth = getAuth();
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
