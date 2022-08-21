07ec99f1b35f72d4e57836c1bc17630081068fcc
winovation <gwon.innovation@gmail.com>
2022-08-21 21:38:57 +0900

    feat: signUp 함수 추가

---
+++ js/signUp.js
@@ -0,0 +1,20 @@
+import { app, analytics } from './initialize.js';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+
+export function signUpWithEmailPassword(email, password) {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
