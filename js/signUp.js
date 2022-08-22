f7792dcca7d82c1caeb79f6b080e37133b3488ad
goodjam92 <bbackoo@naver.com>
2022-08-22 23:47:46 +0900

    fix: 회원가입 코드 js 파일로 옮겨서 기본 기능 구현

--- js/signUp.js
+++ js/signUp.js
@@ -8,3 +8,31 @@ export async function signUpWithEmailPassword(email, password) {
   const auth = getAuth();
   return await createUserWithEmailAndPassword(auth, email, password);
 }
+
+function signUp() {
+  const result = signUpWithEmailPassword('test@test.com', 'testtest')
+    .then(() => {
+      alert('회원가입 성공');
+      // 화면이동
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      switch (errorCode) {
+        case 'auth/email-already-in-use':
+          alert(`이미 가입된 이메일입니다. 다른 이메일을 이용해주세요.`);
+          break;
+      }
+    });
+}
+
+const signUpButton = document.getElementById('signUpbtn');
+
+signUpButton.addEventListener('click', handleSignUpSubmit);
+
+function handleSignUpSubmit(event) {
+  event.preventDefault();
+  const signUpEmail = document.getElementById('signUpEmail').value;
+  const signUpPassword = document.getElementById('signUpPassword').value;
+  signUp(signUpEmail, signUpPassword);
+}
