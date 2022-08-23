d362c81cb059292a8e7bf91ae0d2442da2563a0b
goodjam92 <bbackoo@naver.com>
2022-08-23 23:23:20 +0900

    fix: 예외 처리 추가

--- js/signUp.js
+++ js/signUp.js
@@ -22,8 +22,31 @@ function signUp(getUserEmail, getUserPassword) {
       const errorMessage = error.message;
       switch (errorCode) {
         case 'auth/email-already-in-use':
+          document.getElementById('signUpEmail').focus();
           alert(`이미 가입된 이메일입니다. 다른 이메일을 이용해주세요.`);
           break;
+        case 'auth/invalid-argument':
+          alert(
+            `${errorCode}: 예기치 못한 문제가 발생하였습니다. 관리자에게 문의하십시오.`,
+          );
+          break;
+        case 'auth/invalid-creation-time':
+          alert(
+            `${errorCode}: 예기치 못한 문제가 발생하였습니다. 관리자에게 문의하십시오.`,
+          );
+        case 'auth/invalid-email':
+          document.getElementById('signUpEmail').focus();
+          alert(`메일 형식에 맞지 않습니다. 메일 정보를 다시 확인해주세요.`);
+          break;
+        // 동작하지가 않슴다..
+        /*         case 'auth/invalid-password': 
+          document.getElementById('signUpPassword').focus();
+          alert(
+            `비밀번호가 6자 이상이어야 합니다. 비밀번호를 다시 입력해주세요.`,
+          );
+          break; */
+        default:
+          alert(`알 수 없는 오류가 발생하였습니다. 관리자에게 문의하십시오.`);
       }
     });
 }
@@ -35,17 +58,28 @@ signUpButton.addEventListener('click', handleSignUpSubmit);
 function handleSignUpSubmit(event) {
   event.preventDefault();
   const signUpEmail = document.getElementById('signUpEmail').value;
-  const signUpPassword = signUpPasswordCheck();
-  signUp(signUpEmail, signUpPassword);
+  if (!signUpEmail) {
+    printNoneValue();
+  } else {
+    const signUpPassword = signUpPasswordCheck();
+    signUp(signUpEmail, signUpPassword);
+  }
 }
 
 function signUpPasswordCheck() {
   const password = document.getElementById('signUpPassword').value;
   const passwordCheck = document.getElementById('signUpPasswordCheck').value;
+
   if (password != passwordCheck) {
     document.getElementById('signUpPasswordCheck').focus();
-    alert(`비밀번호가 서로 다릅니다. 다시 확인해주세요.`);
-  } else {
-    return password;
-  }
+    alert('비밀번호가 서로 다릅니다. 다시 확인해주세요.');
+    throw new Error('different-password');
+  } else if (!password) {
+    printNoneValue();
+  } else return password;
+}
+
+function printNoneValue() {
+  alert('입력되지 않은 정보가 있습니다.');
+  throw new Error('none-value-in-input');
 }
