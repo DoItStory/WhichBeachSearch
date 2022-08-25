4f71ec96f6ce57a096b90e064e5612f8acd20b41
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2022-08-25 23:09:07 +0900

    WHIC-28 회원가입 예외처리 추가 (#16)
    * fix: 예외 처리 추가
    
    * fix: 예외처리 코드 수정
    - 입력 된 값을 전체적으로 검토하는 함수 추가 (try, catch 구성)
    - email 입력된 값 확인하는 함수 추가
    - password 입력 값 확인 및 올바른 값인지 확인하는 함수 추가
    
    * style: function 아래로 변수 및 수행 함수 이동
    
    * fix: 에러 메세지 js 파일로 분류
    - 에러 메세지 추가 및 삭제, 관리를 위한 분류
    
    * rafactor: signUp 함수 에러 메세지 알람 발생 기능 함수로 처리

--- js/signUp.js
+++ js/signUp.js
@@ -3,6 +3,7 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { ERROR } from './error.js';
 
 export async function signUpWithEmailPassword(email, password) {
   const auth = getAuth();
@@ -20,32 +21,69 @@ function signUp(getUserEmail, getUserPassword) {
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      switch (errorCode) {
-        case 'auth/email-already-in-use':
-          alert(`이미 가입된 이메일입니다. 다른 이메일을 이용해주세요.`);
-          break;
-      }
+      signUpErrorPrint(errorCode);
     });
 }
 
-const signUpButton = document.getElementById('signUpbtn');
-
-signUpButton.addEventListener('click', handleSignUpSubmit);
+function signUpErrorPrint(errorCode) {
+  switch (errorCode) {
+    case 'auth/email-already-in-use':
+      document.getElementById('signUpEmail').focus();
+      alert(ERROR.CHECK_MAIL_DUPLICATES);
+      break;
+    case 'auth/invalid-argument':
+      alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
+      break;
+    case 'auth/invalid-creation-time':
+      alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
+    case 'auth/invalid-email':
+      document.getElementById('signUpEmail').focus();
+      alert(ERROR.MAIL_DOESNT_FIT);
+      break;
+    case 'auth/weak-password':
+      document.getElementById('signUpPassword').focus();
+      alert(ERROR.CONFIRM_PASSWORD_DIGITS);
+      break;
+    default:
+      alert();
+  }
+}
 
 function handleSignUpSubmit(event) {
   event.preventDefault();
   const signUpEmail = document.getElementById('signUpEmail').value;
-  const signUpPassword = signUpPasswordCheck();
-  signUp(signUpEmail, signUpPassword);
-}
-
-function signUpPasswordCheck() {
   const password = document.getElementById('signUpPassword').value;
   const passwordCheck = document.getElementById('signUpPasswordCheck').value;
-  if (password != passwordCheck) {
-    document.getElementById('signUpPasswordCheck').focus();
-    alert(`비밀번호가 서로 다릅니다. 다시 확인해주세요.`);
-  } else {
-    return password;
+
+  const result = signUpCheck(signUpEmail, password, passwordCheck);
+  if (result) signUp(signUpEmail, password);
+}
+
+// 회원 가입 정보 값이 비어있는지 확인
+function signUpCheck(writtenEmail, writtenPassword, writtenPasswordCheck) {
+  try {
+    signUpEmailCheck(writtenEmail);
+    signUpPasswordCheck(writtenPassword, writtenPasswordCheck);
+    return true;
+  } catch (e) {
+    alert(`${e.message}`);
+    return false;
   }
 }
+
+function signUpEmailCheck(inputEmail) {
+  if (!inputEmail) {
+    throw new Error(ERROR.EMPTY_VALUE_IN_INPUT);
+  } else return true;
+}
+
+function signUpPasswordCheck(inputPassword, inputPasswordCheck) {
+  if (!inputPassword || !inputPasswordCheck) {
+    throw new Error(ERROR.EMPTY_VALUE_IN_INPUT);
+  } else if (inputPassword !== inputPasswordCheck) {
+    throw new Error(ERROR.DIFFERENT_PASSWORD);
+  } else return true;
+}
+
+const signUpButton = document.getElementById('signUpbtn');
+signUpButton.addEventListener('click', handleSignUpSubmit);
