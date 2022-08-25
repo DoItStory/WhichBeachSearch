396d43542d6b0de6452b2c7c036f1ac507ed5e1d
goodjam92 <bbackoo@naver.com>
2022-08-25 23:01:51 +0900

    rafactor: signUp 함수 에러 메세지 알람 발생 기능 함수로 처리

--- js/signUp.js
+++ js/signUp.js
@@ -21,28 +21,32 @@ function signUp(getUserEmail, getUserPassword) {
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      switch (errorCode) {
-        case 'auth/email-already-in-use':
-          document.getElementById('signUpEmail').focus();
-          alert(ERROR.CHECK_MAIL_DUPLICATES);
-          break;
-        case 'auth/invalid-argument':
-          alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
-          break;
-        case 'auth/invalid-creation-time':
-          alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
-        case 'auth/invalid-email':
-          document.getElementById('signUpEmail').focus();
-          alert(ERROR.MAIL_DOESNT_FIT);
-          break;
-        case 'auth/weak-password':
-          document.getElementById('signUpPassword').focus();
-          alert(ERROR.CONFIRM_PASSWORD_DIGITS);
-          break;
-        default:
-          alert();
-      }
+      signUpErrorPrint(errorCode);
     });
+}
+
+function signUpErrorPrint(errorCode) {
+  switch (errorCode) {
+    case 'auth/email-already-in-use':
+      document.getElementById('signUpEmail').focus();
+      alert(ERROR.CHECK_MAIL_DUPLICATES);
+      break;
+    case 'auth/invalid-argument':
+      alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
+      break;
+    case 'auth/invalid-creation-time':
+      alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
+    case 'auth/invalid-email':
+      document.getElementById('signUpEmail').focus();
+      alert(ERROR.MAIL_DOESNT_FIT);
+      break;
+    case 'auth/weak-password':
+      document.getElementById('signUpPassword').focus();
+      alert(ERROR.CONFIRM_PASSWORD_DIGITS);
+      break;
+    default:
+      alert();
+  }
 }
 
 function handleSignUpSubmit(event) {
