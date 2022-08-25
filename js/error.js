e612aec2b99da55a684a63a7a624e026fdd9dd93
goodjam92 <bbackoo@naver.com>
2022-08-25 22:55:01 +0900

    fix: 에러 메세지 js 파일로 분류
    - 에러 메세지 추가 및 삭제, 관리를 위한 분류

---
+++ js/error.js
@@ -0,0 +1,10 @@
+export var ERROR = {
+  DIFFERENT_PASSWORD: '비밀번호가 서로 다릅니다. 다시 입력해주세요.',
+  EMPTY_VALUE_IN_INPUT:
+    '비어있는 정보가 있습니다. 양식을 다시 한 번 확인해주세요.',
+  MAIL_DOESNT_FIT: `메일 형식에 맞지 않습니다. 메일 정보를 다시 확인해주세요.`,
+  CONFIRM_PASSWORD_DIGITS: `비밀번호가 6자 이상이어야 합니다. 비밀번호를 다시 입력해주세요.`,
+  CHECK_MAIL_DUPLICATES: `이미 가입된 이메일입니다. 다른 이메일을 이용해주세요.`,
+  UNKNOWN_ERROR: `알 수 없는 오류가 발생하였습니다. 관리자에게 문의하십시오.`,
+  ADMIN_ERROR: `예기치 못한 문제가 발생하였습니다. 관리자에게 문의하십시오.`,
+};
--- js/signUp.js
+++ js/signUp.js
@@ -3,6 +3,7 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { ERROR } from './error.js';
 
 export async function signUpWithEmailPassword(email, password) {
   const auth = getAuth();
@@ -23,29 +24,23 @@ function signUp(getUserEmail, getUserPassword) {
       switch (errorCode) {
         case 'auth/email-already-in-use':
           document.getElementById('signUpEmail').focus();
-          alert(`이미 가입된 이메일입니다. 다른 이메일을 이용해주세요.`);
+          alert(ERROR.CHECK_MAIL_DUPLICATES);
           break;
         case 'auth/invalid-argument':
-          alert(
-            `${errorCode}: 예기치 못한 문제가 발생하였습니다. 관리자에게 문의하십시오.`,
-          );
+          alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
           break;
         case 'auth/invalid-creation-time':
-          alert(
-            `${errorCode}: 예기치 못한 문제가 발생하였습니다. 관리자에게 문의하십시오.`,
-          );
+          alert(`${errorCode}: ${ERROR.ADMIN_ERROR}`);
         case 'auth/invalid-email':
           document.getElementById('signUpEmail').focus();
-          alert(`메일 형식에 맞지 않습니다. 메일 정보를 다시 확인해주세요.`);
+          alert(ERROR.MAIL_DOESNT_FIT);
           break;
         case 'auth/weak-password':
           document.getElementById('signUpPassword').focus();
-          alert(
-            `비밀번호가 6자 이상이어야 합니다. 비밀번호를 다시 입력해주세요.`,
-          );
+          alert(ERROR.CONFIRM_PASSWORD_DIGITS);
           break;
         default:
-          alert(`알 수 없는 오류가 발생하였습니다. 관리자에게 문의하십시오.`);
+          alert();
       }
     });
 }
@@ -74,19 +69,15 @@ function signUpCheck(writtenEmail, writtenPassword, writtenPasswordCheck) {
 
 function signUpEmailCheck(inputEmail) {
   if (!inputEmail) {
-    throw new Error(
-      '비어있는 정보가 있습니다. 양식을 다시 한 번 확인해주세요.',
-    );
+    throw new Error(ERROR.EMPTY_VALUE_IN_INPUT);
   } else return true;
 }
 
 function signUpPasswordCheck(inputPassword, inputPasswordCheck) {
   if (!inputPassword || !inputPasswordCheck) {
-    throw new Error(
-      '비어있는 정보가 있습니다. 양식을 다시 한 번 확인해주세요.',
-    );
+    throw new Error(ERROR.EMPTY_VALUE_IN_INPUT);
   } else if (inputPassword !== inputPasswordCheck) {
-    throw new Error('비밀번호가 서로 다릅니다. 다시 입력해주세요.');
+    throw new Error(ERROR.DIFFERENT_PASSWORD);
   } else return true;
 }
 
