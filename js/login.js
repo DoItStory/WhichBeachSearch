d1650d1b40b4fdfd67a9784cc072d277ad4841a8
goodjam92 <bbackoo@naver.com>
2022-08-27 22:03:52 +0900

    feat: 기본 로그인 기능 추가

--- js/login.js
+++ js/login.js
@@ -4,20 +4,32 @@ import {
   signInWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
-async function loginWithEmailAndPassword(email, password) {
+async function logInWithEmailAndPassword(email, password) {
   const auth = getAuth();
   return await signInWithEmailAndPassword(auth, email, password);
 }
 
 function logIn(userEmail, userPassword) {
-  const result = loginWithEmailAndPassword(userEmail, userPassword)
+  const result = logInWithEmailAndPassword(userEmail, userPassword)
     .then(userCredential => {
       // Signed in
       const user = userCredential.user;
+      alert('환영합니다!');
+      window.location.href = 'http://127.0.0.1:5500/pages/main/main.html';
       // ...
     })
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
     });
+}
+
+const logInBtn = document.getElementById('loginBtn');
+logInBtn.addEventListener('click', handleLogInBtn);
+
+function handleLogInBtn(event) {
+  event.preventDefault();
+  const getLogInId = document.getElementById('user-id').value;
+  const getLogInPassword = document.getElementById('user-pw').value;
+  logIn(getLogInId, getLogInPassword);
 }
