61c6375c74b77612a26cc119e0ee586d0dceee5a
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2022-08-27 23:19:17 +0900

    WHIC-29 로그인 기능 구현 및 예외처리 추가 (#17)
    * feat: logIn 함수 추가
    
    * feat: 기본 로그인 기능 추가
    
    * refactor: login 일부 요소 id, class명 변경
    
    * fix: 예외 처리 추가
    
    * fix: 로그인 정보 자동저장 해제 및 required 삭제
    
    * fix: 에러 확인용 테스트 코드 삭제 및 불필요한 주석 삭제

---
+++ js/login.js
@@ -0,0 +1,53 @@
+import { initializeFirebase } from './initialize.js';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { ERROR } from './error.js';
+
+async function logInWithEmailAndPassword(email, password) {
+  const auth = getAuth();
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+function logIn(userEmail, userPassword) {
+  const result = logInWithEmailAndPassword(userEmail, userPassword)
+    .then(userCredential => {
+      const user = userCredential.user;
+      alert('환영합니다!');
+      window.location.href = 'http://127.0.0.1:5500/pages/main/main.html';
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      logInErrorPrint(errorCode);
+    });
+}
+
+function logInErrorPrint(errorCode) {
+  switch (errorCode) {
+    case 'auth/invalid-email':
+    case 'auth/user-not-found':
+    case 'auth/wrong-password':
+      alert(ERROR.INCORRECT_LOGIN_INFO);
+      break;
+    case 'auth/internal-error':
+      alert(ERROR.INTERNAL_ERROR);
+      break;
+    case 'auth/too-many-requests':
+      alert(ERROR.TOO_MANY_REQUESTS);
+      break;
+    default:
+      alert();
+  }
+}
+
+const logInBtn = document.getElementById('loginBtn');
+logInBtn.addEventListener('click', handleLogInBtn);
+
+function handleLogInBtn(event) {
+  event.preventDefault();
+  const getLogInId = document.getElementById('login-id').value;
+  const getLogInPassword = document.getElementById('login-pw').value;
+  logIn(getLogInId, getLogInPassword);
+}
