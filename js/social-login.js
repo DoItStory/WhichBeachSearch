5b89c96f9d8e8e3e8c448ded61cf8172a76c7ce5
goodjam92 <bbackoo@naver.com>
2022-08-28 17:56:45 +0900

    feat: 구글 로그인 기능 추가

---
+++ js/social-login.js
@@ -0,0 +1,37 @@
+import { initializeFirebase } from './initialize.js';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+
+async function googleLogInResult() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  return await signInWithPopup(auth, provider);
+}
+
+function handleGoogleLogInBtn(event) {
+  event.preventDefault();
+  const result = googleLogInResult()
+    .then(result => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+const googleLogInbtn = document.getElementById('google-login-btn');
+googleLogInbtn.addEventListener('click', handleGoogleLogInBtn);
