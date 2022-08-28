e28743cca01d9bb24684f20f8a3993afb7773907
goodjam92 <bbackoo@naver.com>
2022-08-28 21:43:46 +0900

    feat: facebook 로그인 기능 추가

--- js/social-login.js
+++ js/social-login.js
@@ -3,8 +3,10 @@ import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
+  FacebookAuthProvider,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
+// Google 로그인
 async function googleLogInResult() {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
@@ -33,5 +35,38 @@ function handleGoogleLogInBtn(event) {
       // ...
     });
 }
-const googleLogInbtn = document.getElementById('google-login-btn');
-googleLogInbtn.addEventListener('click', handleGoogleLogInBtn);
+
+// Facebook 로그인
+async function facebookLogInResult() {
+  const provider = new FacebookAuthProvider();
+  const auth = getAuth();
+  return await signInWithPopup(auth, provider);
+}
+
+function handleFacebookLogInBtn(event) {
+  event.preventDefault();
+  const result = facebookLogInResult()
+    .then(result => {
+      // The signed-in user info.
+      const user = result.user;
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+const googleLogIn = document.getElementById('google-login');
+googleLogIn.addEventListener('click', handleGoogleLogInBtn);
+
+const facebookLogIn = document.getElementById('facebook-login');
+facebookLogIn.addEventListener('click', handleFacebookLogInBtn);
