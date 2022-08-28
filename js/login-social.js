e884ee3016ac3009a13c0106ce251ccdb8fed04d
goodjam92 <bbackoo@naver.com>
2022-08-28 23:39:03 +0900

    refactor: 불필요 주석 삭제 및 코드 위치 정리

--- js/login-social.js
+++ js/login-social.js
@@ -18,7 +18,6 @@ function handleGoogleLogInBtn(event) {
   event.preventDefault();
   const result = googleLogInResult()
     .then(result => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       // The signed-in user info.
@@ -27,9 +26,7 @@ function handleGoogleLogInBtn(event) {
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       socialLogInErrorPrint(errorCode);
     });
@@ -46,28 +43,18 @@ function handleFacebookLogInBtn(event) {
   event.preventDefault();
   const result = facebookLogInResult()
     .then(result => {
-      // The signed-in user info.
-      const user = result.user;
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       const credential = FacebookAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
+      const token = credential.accessToken;
+      const user = result.user;
     })
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = FacebookAuthProvider.credentialFromError(error);
       socialLogInErrorPrint(errorCode);
     });
 }
-
-const googleLogIn = document.getElementById('google-login');
-googleLogIn.addEventListener('click', handleGoogleLogInBtn);
-
-const facebookLogIn = document.getElementById('facebook-login');
-facebookLogIn.addEventListener('click', handleFacebookLogInBtn);
 
 function socialLogInErrorPrint(errorCode) {
   switch (errorCode) {
@@ -85,3 +72,9 @@ function socialLogInErrorPrint(errorCode) {
       alert(ERROR.UNKNOWN_ERROR);
   }
 }
+
+const googleLogIn = document.getElementById('google-login');
+googleLogIn.addEventListener('click', handleGoogleLogInBtn);
+
+const facebookLogIn = document.getElementById('facebook-login');
+facebookLogIn.addEventListener('click', handleFacebookLogInBtn);
