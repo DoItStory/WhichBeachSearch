561d537387f38fcbcbc89c7adedb6a44151b71f6
goodjam92 <bbackoo@naver.com>
2022-08-31 23:50:44 +0900

    fix: 북마크 진입 시 회원, 비회원 상황에서의 동작 추가

--- js/nav.js
+++ js/nav.js
@@ -10,16 +10,17 @@ function getUserProfile() {
   const auth = getAuth();
   onAuthStateChanged(auth, user => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
       const uid = user.uid;
       const email = user.email;
-      console.log(`${uid}, ${email}`);
-      // ...
+      window.location.href =
+        'http://127.0.0.1:5500/pages/bookmark/bookmark.html';
     } else {
-      console.log(`i'm guest`);
-      // User is signed out
-      // ...
+      const askSignUp = confirm(
+        '이 페이지는 로그인을 하셔야 사용이 가능합니다. 로그인 하시겠습니까?',
+      );
+      if (askSignUp) {
+        window.location.href = 'http://127.0.0.1:5500/pages/login/login.html';
+      } else return;
     }
   });
 }
@@ -27,8 +28,7 @@ function getUserProfile() {
 const bookMarkBtn = document.getElementById('bookmark-icon');
 bookMarkBtn.addEventListener('click', getUserProfile);
 
-// test용 로그아웃 버튼 함수
-
+// test용 로그아웃 버튼
 const logOutBtn = document.getElementById('logout-btn');
 logOutBtn.addEventListener('click', logout);
 
