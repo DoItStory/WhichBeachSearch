ff41f47f40f485ee1b4c9e6894a9da2400b3408c
goodjam92 <bbackoo@naver.com>
2022-09-01 23:21:08 +0900

    feat: 회원일 경우 메인 화면에서 즐겨찾기 아이콘 표시 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 import { initializeFirebase } from './initialize.js';
 import {
   getAuth,
+  onAuthStateChanged,
   // test용 로그아웃 버튼 생성
   signOut,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+
+const bookmarkBtn = document.querySelector('.search__bookmark-btn');
+const HIDDEN_CLASSNAME = 'hidden';
+
+const auth = getAuth();
+onAuthStateChanged(auth, user => {
+  if (user) {
+    bookmarkBtn.classList.remove(HIDDEN_CLASSNAME);
+    const uid = user.uid;
+    const email = user.email;
+  } else return;
+});
 
 // test용 로그아웃 버튼
 const logOutBtn = document.getElementById('logout-btn');
