c79b60c72bb0c7387f9c3a652d8ea77d0c48bd02
goodjam92 <bbackoo@naver.com>
2022-09-01 22:26:21 +0900

    fix: bookmark 페이지 진입 시 회원 여부 판단 기능 추가
    - 링크 검색으로 진입, nav 버튼으로 진입 시 모두 체크

---
+++ js/bookmark.js
@@ -0,0 +1,20 @@
+import { initializeFirebase } from './initialize.js';
+import {
+  getAuth,
+  onAuthStateChanged,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+
+const auth = getAuth();
+onAuthStateChanged(auth, user => {
+  if (user) {
+    const uid = user.uid;
+    const email = user.email;
+  } else {
+    const askSignUp = confirm(
+      '이 기능은 로그인을 하셔야 사용이 가능합니다. 로그인 하시겠습니까?',
+    );
+    if (askSignUp) {
+      window.location.href = 'http://127.0.0.1:5500/pages/login/login.html';
+    } else history.back();
+  }
+});
