7eed91e46ffc2f705639d02f3475f75f5c40d1e3
goodjam92 <bbackoo@naver.com>
2022-09-03 16:30:48 +0900

    feat: 메인 화면 즐겨 찾기 추가 버튼 기능 함수 추가

--- pages/main/js/main.js
+++ pages/main/js/main.js
@@ -9,17 +9,41 @@ import {
   signOut,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
+import {
+  collection,
+  addDoc,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
+
 const bookmarkBtn = document.querySelector('.search__bookmark-btn');
 const HIDDEN_CLASSNAME = 'hidden';
+let USER_UID;
 
 const auth = getAuth();
 onAuthStateChanged(auth, user => {
   if (user) {
     bookmarkBtn.classList.remove(HIDDEN_CLASSNAME);
-    const uid = user.uid;
-    const email = user.email;
+    USER_UID = user.uid;
   } else return;
 });
+
+// firestore
+const db = fireStoreInitialize();
+async function handleBookmarkBtn() {
+  try {
+    const docRef = await addDoc(collection(db, 'Bookmark'), {
+      name: '해운대 해수욕장',
+      address: '부산광역시 해운대구 우동',
+      uid: USER_UID,
+    });
+    // 테스트 확인용 console.log (기능 구현 완료 시 비동기 형식과 같이 삭제)
+    console.log('Document written with ID: ', docRef);
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+}
+
+const addBookmarkBtn = document.getElementById('bookmark-btn');
+addBookmarkBtn.addEventListener('click', handleBookmarkBtn);
 
 // test용 로그아웃 버튼
 const logOutBtn = document.getElementById('logout-btn');
