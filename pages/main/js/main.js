199ff3e08e81a5152f850bb40782041cfa6f2856
goodjam92 <bbackoo@naver.com>
2022-09-04 17:37:33 +0900

    feat: 유저의 uid를 검색하여 저장소가 있는지 확인하는 함수 추가

--- pages/main/js/main.js
+++ pages/main/js/main.js
@@ -12,6 +12,9 @@ import {
 import {
   collection,
   addDoc,
+  query,
+  where,
+  getDocs,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 
 const bookmarkBtn = document.querySelector('.search__bookmark-btn');
@@ -28,18 +31,34 @@ onAuthStateChanged(auth, user => {
 
 // firestore
 const db = fireStoreInitialize();
+let docRefId;
+
 async function handleBookmarkBtn() {
-  try {
-    const docRef = await addDoc(collection(db, 'Bookmark'), {
-      name: '해운대 해수욕장',
-      address: '부산광역시 해운대구 우동',
-      uid: USER_UID,
-    });
-    // 테스트 확인용 console.log (기능 구현 완료 시 비동기 형식과 같이 삭제)
-    console.log('Document written with ID: ', docRef);
-  } catch (e) {
-    console.error('Error adding document: ', e);
+  await checkUserStore();
+  if (!docRefId) {
+    // todo: 새로운 문서 생성하여 데이터 입력
+    try {
+      const docRef = await addDoc(collection(db, 'Bookmark'), {
+        name: '해운대 해수욕장',
+        address: '부산광역시 해운대구 우동',
+        uid: USER_UID,
+      });
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  } else {
+    // todo: 유저의 데이터 저장소에 즐겨찾기 추가 기능
   }
+}
+
+// 유저의 uid가 포함된 문서가 있는지 찾는 함수
+async function checkUserStore() {
+  const q = query(collection(db, 'Bookmark'), where('uid', '==', USER_UID));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach(doc => {
+    docRefId = doc.id;
+  });
+  // 해당 유저의 저장소가 1개가 아닌 모종의 이유로 1개 이상일 경우를 대비하여 에러 작성필요할듯?
 }
 
 const addBookmarkBtn = document.getElementById('bookmark-btn');
