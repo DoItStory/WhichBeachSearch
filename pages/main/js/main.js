ed0e6f29f70ee2ff681a3d97d29177d95b65770f
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2022-09-04 17:45:31 +0900

    WHIC-35 즐겨찾기 등록 기능 추가 (#25)
    * chore: firestore  추가 및 초기화 함수 설정
    
    * feat: 메인 화면 즐겨 찾기 추가 버튼 기능 함수 추가
    
    * feat: 유저의 uid를 검색하여 저장소가 있는지 확인하는 함수 추가
    
    * feat: 새로운 문서 생성 함수 추가
    
    * feat: 문서가 존재하는 유저의 필드에 데이터 추가하는 함수 추가

--- pages/main/js/main.js
+++ pages/main/js/main.js
@@ -1,4 +1,7 @@
-import { initializeFirebase } from '../../../js/initialize.js';
+import {
+  initializeFirebase,
+  fireStoreInitialize,
+} from '../../../js/initialize.js';
 import {
   getAuth,
   onAuthStateChanged,
@@ -6,17 +9,83 @@ import {
   signOut,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+  arrayUnion,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
+
 const bookmarkBtn = document.querySelector('.search__bookmark-btn');
 const HIDDEN_CLASSNAME = 'hidden';
+let USER_UID;
 
 const auth = getAuth();
 onAuthStateChanged(auth, user => {
   if (user) {
     bookmarkBtn.classList.remove(HIDDEN_CLASSNAME);
-    const uid = user.uid;
-    const email = user.email;
+    USER_UID = user.uid;
   } else return;
 });
+
+// firestore
+const db = fireStoreInitialize();
+let docRefId;
+
+async function handleBookmarkBtn() {
+  await checkUserStore();
+  if (!docRefId) {
+    createUserDoc();
+  } else {
+    addDataInField(docRefId);
+  }
+}
+// 새로운 문서(doc) 생성 함수
+async function createUserDoc() {
+  try {
+    const docData = [
+      {
+        name: '해운대 해수욕장',
+        address: '부산광역시 해운대구 우동',
+      },
+    ];
+    const docRef = await addDoc(collection(db, 'Bookmark'), {
+      docData,
+      uid: USER_UID,
+    });
+    docRefId = docRef.id;
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+}
+
+// 즐겨찾기 정보(map) 추가 함수
+async function addDataInField(docRefId) {
+  const docRef = doc(db, 'Bookmark', docRefId);
+  await updateDoc(docRef, {
+    docData: arrayUnion({
+      name: '명사십리 해수욕장',
+      address: '전라남도 완도군',
+    }),
+  });
+}
+
+// 유저의 uid가 포함된 문서가 있는지 찾는 함수
+async function checkUserStore() {
+  const q = query(collection(db, 'Bookmark'), where('uid', '==', USER_UID));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach(doc => {
+    docRefId = doc.id;
+  });
+  // 해당 유저의 저장소가 1개가 아닌 모종의 이유로 1개 이상일 경우를 대비하여 에러 작성필요할듯?
+}
+
+const addBookmarkBtn = document.getElementById('bookmark-btn');
+addBookmarkBtn.addEventListener('click', handleBookmarkBtn);
 
 // test용 로그아웃 버튼
 const logOutBtn = document.getElementById('logout-btn');
