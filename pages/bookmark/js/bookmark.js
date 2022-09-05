872e6c8041af69c7810d6aa6475b403979b91726
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2022-09-05 21:26:52 +0900

    WHIC-36 즐겨찾기 리스트 불러와서 화면에 표시 (#26)
    * fix: 북마크 아이콘 앵커 -> 버튼으로 변경
    
    * feat: 유저 정보 파악 함수와 즐겨찾기 목록 생성 기본 함수 추가
    
    * fix: 필드 안에 배열 정보 찾아서 변수에 담는 기능 추가
    
    * feat: 필드에 있는 데이터를 화면에 리스트로 표시 하는 함수 추가
    
    * refactor: uid 변수명 변경, 필드 데이터명 변경
    
    * fix: 필요 없는 html 요소 제거
    
    * refactor: 필요 없는 전역 변수 및 모듈 삭제
    
    * fix: 해수욕장 이름을 오름차순으로 정렬 추가

--- pages/bookmark/js/bookmark.js
+++ pages/bookmark/js/bookmark.js
@@ -1,14 +1,31 @@
-import { initializeFirebase } from '../../../js/initialize.js';
+import {
+  initializeFirebase,
+  fireStoreInitialize,
+} from '../../../js/initialize.js';
 import {
   getAuth,
   onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 
+const bookmarkList = document.getElementById('bookmark-list');
+const BOOKMARK_ITEM_CLASSNAME = 'bookmark__item';
+const BOOKMARK_DIV_CLASS = 'bookmark__info';
+const BOOKMARK_BTN_CLASS = 'bookmark__icon';
+const BOOKMARK_ICON_CLASS = 'fa-solid';
+const BOOKMARK_ICON_CLASS2 = 'fa-star';
+
+const db = fireStoreInitialize();
 const auth = getAuth();
+
 onAuthStateChanged(auth, user => {
   if (user) {
-    const uid = user.uid;
-    const email = user.email;
+    getUserData(user.uid);
   } else {
     const askSignUp = confirm(
       '이 기능은 로그인을 하셔야 사용이 가능합니다. 로그인 하시겠습니까?',
@@ -18,3 +35,47 @@ onAuthStateChanged(auth, user => {
     } else history.back();
   }
 });
+
+async function getUserData(uid) {
+  const q = query(collection(db, 'Bookmark'), where('uid', '==', uid));
+  const querySnapshot = await getDocs(q);
+  let bookmarkListData = [];
+
+  querySnapshot.forEach(doc => {
+    bookmarkListData = doc.data().userBookmarkList.sort(listSortByName);
+  });
+  paintBookmarkList(bookmarkListData);
+}
+
+function listSortByName(firstArr, secondArr) {
+  let x = firstArr.name.toLowerCase();
+  let y = secondArr.name.toLowerCase();
+  if (x > y) return 1;
+  if (x < y) return -1;
+  return 0;
+}
+
+function paintBookmarkList(dataList) {
+  for (let data of dataList) {
+    const list = document.createElement('li');
+    list.classList.add(BOOKMARK_ITEM_CLASSNAME);
+    const div = document.createElement('div');
+    div.classList.add(BOOKMARK_DIV_CLASS);
+    const span = document.createElement('span');
+    span.textContent = data.name;
+    const address = document.createElement('address');
+    address.textContent = data.address;
+    const button = document.createElement('button');
+    button.classList.add(BOOKMARK_BTN_CLASS);
+    const icon = document.createElement('i');
+    icon.classList.add(BOOKMARK_ICON_CLASS);
+    icon.classList.add(BOOKMARK_ICON_CLASS2);
+
+    div.appendChild(span);
+    div.appendChild(address);
+    button.appendChild(icon);
+    list.appendChild(div);
+    list.appendChild(button);
+    bookmarkList.appendChild(list);
+  }
+}
