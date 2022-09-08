f6abc5129720533fc584e2a49bec6dfeea170ed9
goodjam92 <bbackoo@naver.com>
2022-09-08 22:53:12 +0900

    fix: bookmark 에러 발생 체크 추가

--- pages/bookmark/js/bookmark.js
+++ pages/bookmark/js/bookmark.js
@@ -18,7 +18,7 @@ import {
   showCircularProgress,
   hideCircularProgress,
 } from '../../../js/circular-progress.js';
-
+import { ERROR } from '../../../js/error.js';
 const bookmarkList = document.getElementById('bookmark-list');
 const BOOKMARK_ITEM_CLASSNAME = 'bookmark__item';
 const BOOKMARK_DIV_CLASS = 'bookmark__info';
@@ -45,81 +45,130 @@ onAuthStateChanged(auth, user => {
 
 async function getUserData(uid) {
   showCircularProgress();
-  const querySnapshot = await getUserDoc(uid);
-  let bookmarkListData = [];
-  querySnapshot.forEach(doc => {
-    bookmarkListData = doc.data().userBookmarkList.sort(listSortByName);
-  });
-  paintBookmarkList(bookmarkListData);
-  hideCircularProgress();
+  const querySnapshot = await getUserDoc(uid)
+    .then(querySnapshot => {
+      let bookmarkListData = [];
+      querySnapshot.forEach(doc => {
+        bookmarkListData = doc.data().userBookmarkList.sort(listSortByName);
+      });
+      paintBookmarkList(bookmarkListData);
+      hideCircularProgress();
+    })
+    .catch(error => {
+      hideCircularProgress();
+      const errorCode = error.code;
+      alert(`${ERROR.UNKNOWN_ERROR} bookmark-error getUserData : ${errorCode}`);
+    });
 }
 
 function listSortByName(firstArr, secondArr) {
-  let x = firstArr.name.toLowerCase();
-  let y = secondArr.name.toLowerCase();
-  if (x > y) return 1;
-  if (x < y) return -1;
-  return 0;
+  try {
+    let x = firstArr.name.toLowerCase();
+    let y = secondArr.name.toLowerCase();
+    if (x > y) return 1;
+    if (x < y) return -1;
+    return 0;
+  } catch (error) {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(
+      `${ERROR.UNKNOWN_ERROR} bookmark-error listSortByName : ${errorCode}`,
+    );
+  }
 }
 
 function paintBookmarkList(dataList) {
-  for (let data of dataList) {
-    const list = document.createElement('li');
-    list.classList.add(BOOKMARK_ITEM_CLASSNAME);
-    const div = document.createElement('div');
-    div.classList.add(BOOKMARK_DIV_CLASS);
-    const span = document.createElement('span');
-    span.textContent = data.name;
-    const address = document.createElement('address');
-    address.textContent = data.address;
-    const button = document.createElement('button');
-    button.classList.add(BOOKMARK_BTN_STYLE_CLASS);
-    button.classList.add(BOOKMARK_BTN_CLASS);
-    const icon = document.createElement('i');
-    icon.classList.add(BOOKMARK_ICON_CLASS);
-    icon.classList.add(BOOKMARK_ICON_CLASS2);
+  try {
+    for (let data of dataList) {
+      const list = document.createElement('li');
+      list.classList.add(BOOKMARK_ITEM_CLASSNAME);
+      const div = document.createElement('div');
+      div.classList.add(BOOKMARK_DIV_CLASS);
+      const span = document.createElement('span');
+      span.textContent = data.name;
+      const address = document.createElement('address');
+      address.textContent = data.address;
+      const button = document.createElement('button');
+      button.classList.add(BOOKMARK_BTN_STYLE_CLASS);
+      button.classList.add(BOOKMARK_BTN_CLASS);
+      const icon = document.createElement('i');
+      icon.classList.add(BOOKMARK_ICON_CLASS);
+      icon.classList.add(BOOKMARK_ICON_CLASS2);
 
-    button.addEventListener('click', getInfoToBeDelete);
-    div.addEventListener('click', goToMainScreen);
+      button.addEventListener('click', getInfoToBeDelete);
+      div.addEventListener('click', goToMainScreen);
 
-    div.appendChild(span);
-    div.appendChild(address);
-    button.appendChild(icon);
-    list.appendChild(div);
-    list.appendChild(button);
-    bookmarkList.appendChild(list);
+      div.appendChild(span);
+      div.appendChild(address);
+      button.appendChild(icon);
+      list.appendChild(div);
+      list.appendChild(button);
+      bookmarkList.appendChild(list);
+    }
+  } catch (error) {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(
+      `${ERROR.UNKNOWN_ERROR} bookmark-error paintBookmarkList : ${errorCode}`,
+    );
   }
 }
 
 function goToMainScreen(event) {
-  const pushBeachName = event.currentTarget.firstElementChild.innerText;
-  const pushBeachAddress = event.currentTarget.lastElementChild.innerText;
-  location.href = `../main/main.html?pushBeachName=${pushBeachName}&pushBeachAddress=${pushBeachAddress}`;
+  showCircularProgress();
+  try {
+    const pushBeachName = event.currentTarget.firstElementChild.innerText;
+    const pushBeachAddress = event.currentTarget.lastElementChild.innerText;
+    location.href = `../main/main.html?pushBeachName=${pushBeachName}&pushBeachAddress=${pushBeachAddress}`;
+  } catch (error) {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(
+      `${ERROR.UNKNOWN_ERROR} bookmark-error goToMainScreen : ${errorCode}`,
+    );
+  }
 }
 
 async function getInfoToBeDelete(event) {
-  const userUid = auth.currentUser.uid;
-  const beachName =
-    event.target.parentElement.parentElement.childNodes[0].childNodes[0]
-      .innerText;
-  const beachList = event.target.parentElement.parentElement;
-  getDocIdAndIndex(userUid, beachName);
-  beachList.remove();
+  try {
+    const userUid = auth.currentUser.uid;
+    const beachName =
+      event.target.parentElement.parentElement.childNodes[0].childNodes[0]
+        .innerText;
+    const beachList = event.target.parentElement.parentElement;
+    getDocIdAndIndex(userUid, beachName);
+    beachList.remove();
+  } catch (error) {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(
+      `${ERROR.UNKNOWN_ERROR} bookmark-error getInfoToBeDelete : ${errorCode}`,
+    );
+  }
 }
 
 async function getDocIdAndIndex(uid, beachName) {
   let docRefId;
   let deleteBookmarkIndex;
   let bookmarkList;
-  const querySnapshot = await getUserDoc(uid);
-  querySnapshot.forEach(doc => {
-    docRefId = doc.id;
-    deleteBookmarkIndex = doc
-      .data()
-      .userBookmarkList.findIndex(beachInfo => beachInfo.name == beachName);
-    bookmarkList = doc.data().userBookmarkList;
-  });
-  deleteBookmark(docRefId, deleteBookmarkIndex, bookmarkList);
+  const querySnapshot = await getUserDoc(uid)
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        docRefId = doc.id;
+        deleteBookmarkIndex = doc
+          .data()
+          .userBookmarkList.findIndex(beachInfo => beachInfo.name == beachName);
+        bookmarkList = doc.data().userBookmarkList;
+      });
+      deleteBookmark(docRefId, deleteBookmarkIndex, bookmarkList);
+    })
+    .catch(error => {
+      hideCircularProgress();
+      const errorCode = error.code;
+      alert(
+        `${ERROR.UNKNOWN_ERROR} bookmark-error getDocIdAndIndex : ${errorCode}`,
+      );
+    });
 }
 
 function getUserDoc(uid) {
@@ -128,13 +177,25 @@ function getUserDoc(uid) {
 }
 
 function deleteBookmark(docRefId, deleteIndex, listArray) {
-  listArray.splice(deleteIndex, 1);
-  upDataDoc(docRefId, listArray);
+  try {
+    listArray.splice(deleteIndex, 1);
+    upDataDoc(docRefId, listArray);
+  } catch (error) {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(
+      `${ERROR.UNKNOWN_ERROR} bookmark-error getInfoToBeDelete : ${errorCode}`,
+    );
+  }
 }
 
 async function upDataDoc(docId, bookmarkList) {
   const BookmarkRef = doc(db, 'Bookmark', docId);
   await updateDoc(BookmarkRef, {
     userBookmarkList: bookmarkList,
+  }).catch(error => {
+    hideCircularProgress();
+    const errorCode = error.code;
+    alert(`${ERROR.UNKNOWN_ERROR} main-error upDataDoc : ${errorCode}`);
   });
 }
