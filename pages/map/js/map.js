56acbb87d6717cfe99468ed19c639859337123a1
goodjam92 <bbackoo@naver.com>
2022-09-11 23:11:57 +0900

    feat: 마커 위에 인포윈도우 창 표시 기능 추가

--- pages/map/js/map.js
+++ pages/map/js/map.js
@@ -1,4 +1,8 @@
 const mapContainer = document.getElementById('map__container');
+const INFO_WINDOW_CLASS = 'info-window';
+const INFO_WINDOW_BEACH_NAME_CLASS = 'info-window__beach-name';
+const INFO_WINDOW_BEACH_ADDRESS_CLASS = 'info-window__beach-address';
+
 const options = {
   center: new kakao.maps.LatLng(35.15723495522564, 129.13830306583512), //지도 중심좌표.
   level: 7,
@@ -20,8 +24,30 @@ function printMarkersMap(beachList) {
       title: beachList[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
       image: createMarkerImage(),
     });
+    createInfoWindows(beachList[i]).open(map, marker);
     marker.setMap(map);
   }
+}
+
+function createInfoWindows(beachData) {
+  const iwContent = printInfoWindow(beachData);
+  const iwPosition = beachData.latlng;
+  const iwRemoveable = true;
+  const infowindow = new kakao.maps.InfoWindow({
+    position: iwPosition,
+    content: iwContent,
+    removable: iwRemoveable,
+  });
+  return infowindow;
+}
+
+function printInfoWindow(beachinfo) {
+  const infoWindowDiv = document.createElement('div');
+  infoWindowDiv.classList.add(INFO_WINDOW_CLASS);
+  const windowBeachNameSpan = document.createElement('span');
+  windowBeachNameSpan.textContent = beachinfo.name;
+  const windowBeachAddress = document.createElement('address');
+  windowBeachAddress.textContent = beachinfo.address;
 }
 
 // 가상의(테스트) 데이터를 받아온다는 설정으로 작성 함.
@@ -29,12 +55,12 @@ function testData() {
   const testData = [];
   const TEST_BEACH_1 = {
     name: '해운대 해수욕장',
-    address: '',
+    address: '부산광역시',
     latlng: new kakao.maps.LatLng(35.1584224777778, 129.160646111111),
   };
   const TEST_BEACH_2 = {
     name: '광안리 해수욕장',
-    address: '',
+    address: '부산광역시',
     latlng: new kakao.maps.LatLng(35.1535555555556, 129.119405555556),
   };
   testData.push(TEST_BEACH_1);
