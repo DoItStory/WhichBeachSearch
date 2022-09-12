ff9e641b7a493114223602dedc026789e08825d4
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2022-09-12 20:16:28 +0900

    WHIC-46 해수욕장 인포 윈도우창 추가 (#34)
    * feat: 마커 위에 인포윈도우 창 표시 기능 추가
    
    * fix: 마커 선택 시 인포 윈도우창 표시되도록 수정
    
    * fIx: 인포 윈도우창의 내용 및 스타일 추가
    
    * refactor: 변수명 수정
    
    * fix: 상세 정보 클릭 시 해당 해수욕장 메인 화면으로 이동 추가
    
    * fix: 카카오 지도 화면이 배경으로 나오도록 수정
    
    * refactor: 변수명 수정 및 필요 없는 변수 삭제

--- pages/map/js/map.js
+++ pages/map/js/map.js
@@ -1,4 +1,5 @@
 const mapContainer = document.getElementById('map__container');
+
 const options = {
   center: new kakao.maps.LatLng(35.15723495522564, 129.13830306583512), //지도 중심좌표.
   level: 7,
@@ -21,7 +22,34 @@ function printMarkersMap(beachList) {
       image: createMarkerImage(),
     });
     marker.setMap(map);
+
+    kakao.maps.event.addListener(marker, 'click', function () {
+      createInfoWindows(beachList[i]).open(map, marker);
+    });
   }
+}
+
+function createInfoWindows(beachData) {
+  const infoWindowContent = `<div class="info-window">
+  <div class="info-title">
+    <h3 class="info-name">
+      ${beachData.name}
+    </h3>
+    <a href='../main/main.html?sendBeachName=${beachData.name}&sendBeachAddress=${beachData.address}'>상세 정보</a>
+  </div>
+  <div class="info-weather">
+    <span>${beachData.temp}</span>
+    <span>${beachData.icon}</span>
+  </div>
+</div>`;
+  const infoWindowPosition = beachData.latlng;
+  const infoWindowRemoveable = true;
+  const infoWindow = new kakao.maps.InfoWindow({
+    position: infoWindowPosition,
+    content: infoWindowContent,
+    removable: infoWindowRemoveable,
+  });
+  return infoWindow;
 }
 
 // 가상의(테스트) 데이터를 받아온다는 설정으로 작성 함.
@@ -29,12 +57,16 @@ function testData() {
   const testData = [];
   const TEST_BEACH_1 = {
     name: '해운대 해수욕장',
-    address: '',
+    address: '부산광역시 해운대구 우동',
+    temp: '현재 27°',
+    icon: '☀️',
     latlng: new kakao.maps.LatLng(35.1584224777778, 129.160646111111),
   };
   const TEST_BEACH_2 = {
     name: '광안리 해수욕장',
-    address: '',
+    address: '부산광역시 해운대구 우동',
+    temp: '현재 25°',
+    icon: '🌧',
     latlng: new kakao.maps.LatLng(35.1535555555556, 129.119405555556),
   };
   testData.push(TEST_BEACH_1);
@@ -42,5 +74,5 @@ function testData() {
   return testData;
 }
 
-const positions = testData();
-printMarkersMap(positions);
+const beachDataBase = testData();
+printMarkersMap(beachDataBase);
