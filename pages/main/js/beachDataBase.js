d02f9d9ca2283b227f544b89a5e82667e5bf799a
goodjam92 <devgoozz@gmail.com>
2022-10-10 16:32:19 +0900

    fix: 데이터 파싱 작업과정 수정 및 파싱 된 데이터 export 작업 추가

--- pages/main/js/beachDataBase.js
+++ pages/main/js/beachDataBase.js
@@ -1,35 +1,46 @@
 const GOOGLE_SPREAD_SHEETS_KEY = '1E7hpYjYFQbxtb6emM3DgdwabrJzwE82_MtY0bzyLUe0';
 
-google.charts.load('current', { packages: ['corechart'] }).then(() => {
-  let query = new google.visualization.Query(
-    `http://spreadsheets.google.com/tq?key=${GOOGLE_SPREAD_SHEETS_KEY}&pub=1`,
-  );
-  query.send(response => {
-    if (response.isError()) {
-      console.error(
-        'Error in query: ' +
-          response.getMessage() +
-          ' ' +
-          response.getDetailedMessage(),
-      );
-      return;
-    }
-
-    let dataTable = response.getDataTable().toJSON();
-    let jsonData = JSON.parse(dataTable);
-    let cols = jsonData.cols.map(col => col.label);
-    let rows = jsonData.rows.map(row => {
-      let newRow;
-
-      row.c.forEach((obj, index) => {
-        if (obj == null || obj == undefined) return;
-        obj[cols[index]] = 'f' in obj ? obj['f'] : obj['v'];
-        ['f', 'v'].forEach(each => delete obj[each]);
-        newRow = { ...newRow, ...obj };
-      });
-      return newRow;
-    });
-    console.log(rows);
-    return rows;
+export async function queryFunc() {
+  return google.charts.load('current', { packages: ['corechart'] }).then(() => {
+    let query = new google.visualization.Query(
+      `http://spreadsheets.google.com/tq?key=${GOOGLE_SPREAD_SHEETS_KEY}&pub=1`,
+      'auto',
+    );
+    return getSpreadSheetsDataList(query);
   });
-});
+}
+
+async function getSpreadSheetsDataList(query) {
+  return new Promise((resolve, reject) => {
+    query.send(response => {
+      if (response.isError()) {
+        console.error(
+          'Error in query: ' +
+            response.getMessage() +
+            ' ' +
+            response.getDetailedMessage(),
+        );
+        return;
+      }
+      resolve(dataListParsing(response));
+    });
+  });
+}
+
+function dataListParsing(response) {
+  let dataTable = response.getDataTable().toJSON();
+  let jsonData = JSON.parse(dataTable);
+  let cols = jsonData.cols.map(col => col.label);
+  let rows = jsonData.rows.map(row => {
+    let newRow;
+
+    row.c.forEach((obj, index) => {
+      if (obj == null || obj == undefined) return; //빈값이 경우 정지
+      obj[cols[index]] = 'f' in obj ? obj['f'] : obj['v'];
+      ['f', 'v'].forEach(each => delete obj[each]);
+      newRow = { ...newRow, ...obj };
+    });
+    return newRow;
+  });
+  return rows;
+}
